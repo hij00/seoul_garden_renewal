1cb04ff047bb0dae310e7f7a93caf408f6ba729e
dnffkdnffk12 <gmlwjdchl82@naver.com>
2022-06-12 20:40:15 +0900

    edit_img

--- js/slide.js
+++ js/slide.js
@@ -3,15 +3,6 @@ const swiper = new Swiper('.swiper', {
 
     speed: 1000,
 
-    pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: 'true',
-    hideOnClick: 'false'
-    
-    },
-
-    
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -20,45 +11,12 @@ const swiper = new Swiper('.swiper', {
     autoplay: {
     delay: 3000,
     pauseOnMouseEnter:'true',
-    }
+    },
       
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    }
 })
-
-const bulEl = document.querySelectorAll('.swiper-pagination-bullet')
-
-bulEl[0].style.backgroundColor = 'white'
-bulEl[1].style.backgroundColor = 'white'
-bulEl[2].style.backgroundColor = 'white'
-bulEl[2].style.marginRight = '70px'
-
-
-
-const bulWrap = document.querySelector('.swiper-pagination')
-bulWrap.style.marginRight = '50px'
-
-// const bullEl = document.querySelector('.swiper-pagination-bullet')
-
-// bullEl.style.backgroundColor = 'white'
-// bullEl.style.opacity = '1.0'
-
-
-// const handleTime = () => {
-//     const bulEl = document.querySelectorAll('.swiper-pagination-bullet-active')
-
-// // bulEl[0].style.marginRight = '200px'
-// // bulEl[1].style.marginRight = '200px'
-// bulEl[0].style.backgroundColor = 'black'
-// bulEl[1].style.backgroundColor = 'black'
-// bulEl[2].style.backgroundColor = 'black'
-
-// const bullEl = document.querySelector('.swiper-pagination-bullet')
-
-// bullEl.style.backgroundColor = 'white'
-// bullEl.style.opacity = '1.0'
-// }
-
-// setInterval(handleTime, 2000)
-
-
 
 
